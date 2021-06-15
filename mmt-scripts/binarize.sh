aa62417f3ad2acdd8819814fcb582489d10768cf
bpopeters <bpeters@coli.uni-saarland.de>
2021-06-15 14:42:59 +0100

    update flores path

--- mmt-scripts/binarize.sh
+++ mmt-scripts/binarize.sh
@@ -1,7 +1,7 @@
 LANGUAGES=$1
 MODEL_PATH=/home/bpop/flores101_mm100_175M/
 DICT=$MODEL_PATH/dict.txt
-FLORES_PATH=/home/bpop/flores101_dataset/
+FLORES_PATH=/home/bpop/flores_tasklangs/
 FAIRSEQ_PATH=..
 
 mkdir -p ../data
