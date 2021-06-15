fc08ad3f7d78ff18e00207d2d78606c5b361a376
bpopeters <bpeters@coli.uni-saarland.de>
2021-06-15 13:00:01 +0100

    add language code-updating script

---
+++ mmt-scripts/flores2m2m.py
@@ -0,0 +1,46 @@
+import sys
+import re
+from os.path import basename, dirname, join
+
+# flores codes seem to be ISO 639-3. M2M codes seem to be ISO 639-1. In lieu
+# of a principled solution, I have written a regular expression that works only
+# for the set of languages included in small tasks 1 and 2.
+
+flores = """
+eng
+est
+hrv
+hun
+mkd
+srp
+ind
+jav
+msa
+snd
+tam
+tgl
+""".split()
+
+m2m = """
+en
+et
+hr
+hu
+mk
+sr
+id
+jv
+ms
+su
+ta
+tl
+""".split()
+
+lookup = dict(zip(flores, m2m))
+
+pattern = '|'.join(sorted(re.escape(k) for k in lookup))  # why the sort?
+
+for line in sys.stdin:
+    fname = basename(line)
+    normalized = re.sub(pattern, lambda m: lookup.get(m.group(0)), fname)
+    sys.stdout.write(join(dirname(line), normalized))
