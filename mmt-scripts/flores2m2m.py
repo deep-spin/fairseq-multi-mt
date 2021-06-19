fc6100aedc186df612327202b93293396ab067f1
bpopeters <bpeters@coli.uni-saarland.de>
2021-06-19 15:49:07 +0100

    use official language code lookup, make conversion bidirectional

--- mmt-scripts/flores2m2m.py
+++ mmt-scripts/flores2m2m.py
@@ -6,39 +6,30 @@ from os.path import basename, dirname, join
 # of a principled solution, I have written a regular expression that works only
 # for the set of languages included in small tasks 1 and 2.
 
-flores = """
-eng
-est
-hrv
-hun
-mkd
-srp
-ind
-jav
-msa
-tam
-tgl
-""".split()
+def read_codes(path, direction):
+    with open(path) as f:
+        lookup = dict()
+        for line in f:
+            name, flores_code, mm_code = line.strip().split("\t")
+            if direction == "mm":
+                lookup[flores_code] = mm_code
+            else:
+                lookup[mm_code] = flores_code
+    return lookup
 
-m2m = """
-en
-et
-hr
-hu
-mk
-sr
-id
-jv
-ms
-ta
-tl
-""".split()
 
-lookup = dict(zip(flores, m2m))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        direction = sys.argv[1]
+        assert direction in ["flores", "mm"]
+    else:
+        direction = "mm"
+    lookup = read_codes(
+        join(dirname(sys.argv[0]), "flores101_codes.txt"), direction
+    )
+    pattern = '|'.join(sorted(re.escape(k) for k in lookup))  # why the sort?
 
-pattern = '|'.join(sorted(re.escape(k) for k in lookup))  # why the sort?
-
-for line in sys.stdin:
-    fname = basename(line)
-    normalized = re.sub(pattern, lambda m: lookup.get(m.group(0)), fname)
-    sys.stdout.write(join(dirname(line), normalized))
+    for line in sys.stdin:
+        fname = basename(line)
+        normalized = re.sub(pattern, lambda m: lookup.get(m.group(0)), fname)
+        sys.stdout.write(join(dirname(line), normalized))
