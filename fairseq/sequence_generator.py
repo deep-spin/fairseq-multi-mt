4104f4d6edd4b3d3ad1e026b36360f43341a48a8
bpopeters <bpeters@coli.uni-saarland.de>
2021-08-07 00:54:53 +0100

    add a MultiPivotEnsembleModel (untested)

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -881,6 +881,155 @@ class EnsembleModel(nn.Module):
             )
 
 
+class MultiPivotEnsembleModel(nn.Module):
+    """
+    Not sure if this should subclass EnsembleModel
+    """
+    def __init__(self, model):
+        # doesn't need to be much more than a wrapper about model, which
+        # should probably be an ensemble model
+        self.model = model
+        self.has_incremental = self.model.has_incremental
+
+    def has_encoder(self):
+        return self.model.has_encoder()
+
+    def has_incremental_states(self):
+        return self.has_incremental
+
+    def max_decoder_positions(self):
+        return self.model.max_decoder_positions()
+
+    @torch.jit.export
+    def forward_encoder(self, net_inputs: List[Dict[str, Tensor]]):
+        if not self.has_encoder():
+            return None
+        # should be self.model.encoder_forward?
+        return [self.model.forward_encoder(net_input)
+                for net_input in net_inputs]
+
+    @torch.jit.export
+    def forward_decoder(
+        self,
+        tokens,
+        encoder_outs: List[Dict[str, List[Tensor]]],
+        incremental_states: List[Dict[str, Dict[str, Optional[Tensor]]]],
+        temperature: float = 1.0,
+        alpha: float = 1.0
+    ):
+        log_probs = []
+        avg_attn: Optional[Tensor] = None
+        encoder_out: Optional[Dict[str, List[Tensor]]] = None
+
+        # can't iterate over models: have to iterate over encoder_outs
+        for i, encoder_out in enumerate(encoder_outs):
+            # decode each model
+            if self.has_incremental_states():
+                # maybe it should be model.decoder_forward, though, because
+                decoder_out = self.model.forward_decoder(
+                    tokens,
+                    encoder_out=encoder_out,
+                    incremental_state=incremental_states[i],
+                )
+            else:
+                # this hasattr might be incorrect...for now, we'll just
+                # assume we're working with the kinds of models that have
+                # encoders and decoders
+                '''
+                if hasattr(self.model, "decoder"):
+                    decoder_out = self.model.decoder.forward(tokens, encoder_out=encoder_out)
+                else:
+                    decoder_out = self.model.forward(tokens)
+                '''
+                decoder_out = self.model.forward_decoder(tokens, encoder_out=encoder_out)
+
+            attn: Optional[Tensor] = None
+            decoder_len = len(decoder_out)
+            if decoder_len > 1 and decoder_out[1] is not None:
+                if isinstance(decoder_out[1], Tensor):
+                    attn = decoder_out[1]
+                else:
+                    attn_holder = decoder_out[1]["attn"]
+                    if isinstance(attn_holder, Tensor):
+                        attn = attn_holder
+                    elif attn_holder is not None:
+                        attn = attn_holder[0]
+                if attn is not None:
+                    attn = attn[:, -1, :]
+
+            decoder_out_tuple = (
+                decoder_out[0][:, -1:, :].div_(temperature),
+                None if decoder_len <= 1 else decoder_out[1],
+            )
+            probs = self.model.get_normalized_probs(
+                decoder_out_tuple, log_probs=True, sample=None, alpha=alpha
+            )
+            probs = probs[:, -1, :]
+            if self.models_size == 1:
+                return probs, attn
+
+            log_probs.append(probs)
+            '''
+            if attn is not None:
+                if avg_attn is None:
+                    avg_attn = attn
+                else:
+                    avg_attn.add_(attn)
+            '''
+
+        avg_probs = torch.logsumexp(torch.stack(log_probs, dim=0), dim=0) - math.log(
+            self.models_size
+        )
+        '''
+        if avg_attn is not None:
+            avg_attn.div_(self.models_size)
+        '''
+        return avg_probs, avg_attn
+
+    @torch.jit.export
+    def reorder_encoder_out(
+        self, encoder_outs: Optional[List[Dict[str, List[Tensor]]]], new_order
+    ):
+        """
+        Reorder encoder output according to *new_order*.
+
+        Args:
+            encoder_out: output from the ``forward()`` method
+            new_order (LongTensor): desired order
+
+        Returns:
+            *encoder_out* rearranged according to *new_order*
+        """
+        '''
+        new_outs: List[Dict[str, List[Tensor]]] = []
+        if not self.has_encoder():
+            return new_outs
+        for i, model in enumerate(self.models):
+            assert encoder_outs is not None
+            new_outs.append(
+                model.encoder.reorder_encoder_out(encoder_outs[i], new_order)
+            )
+        return new_outs
+        '''
+        return self.model.reorder_encoder_out(encoder_outs, new_order)
+
+    @torch.jit.export
+    def reorder_incremental_state(
+        self,
+        incremental_states: List[Dict[str, Dict[str, Optional[Tensor]]]],
+        new_order,
+    ):
+        '''
+        if not self.has_incremental_states():
+            return
+        for i, model in enumerate(self.models):
+            model.decoder.reorder_incremental_state_scripting(
+                incremental_states[i], new_order
+            )
+        '''
+        self.model.reorder_incremental_state(incremental_states, new_order)
+
+
 class SequenceGeneratorWithAlignment(SequenceGenerator):
     def __init__(
         self, models, tgt_dict, left_pad_target=False, print_alignment="hard", **kwargs
