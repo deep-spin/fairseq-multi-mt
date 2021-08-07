5752c58fb2a50e8596236cf19eb71feea1570050
Ben Peters <bpeters@coli.uni-saarland.de>
2021-08-07 11:09:50 +0100

    add stuff for dynalab submission (not sure if this is a good place for it...)

--- mmt-scripts/evaluate_subset.sh
+++ mmt-scripts/evaluate_subset.sh
@@ -18,6 +18,6 @@ for PAIR in $( cat $PAIRS ) ; do
     FLORES_LANG2=$(echo $LANG2 | python $SCRIPTS_PATH/flores2m2m.py flores)
     echo $LANG1 $LANG2
     results_dir=$RESULTS_PATH/hypotheses.$LANG1-$LANG2.$LANG2
-    fairseq-generate $DATA_BIN_PATH --entmax-alpha $ALPHA --batch-size 32 --path $MODEL_PATH --fixed-dictionary $DICT -s $LANG1 -t $LANG2 --remove-bpe 'sentencepiece' --beam $BEAM --task translation_multi_simple_epoch --lang-pairs $FLORES_MODEL_PATH/language_pairs.txt --decoder-langtok --encoder-langtok src --gen-subset test --fp16 --dataset-impl mmap --distributed-world-size 1 --distributed-no-spawn --skip-invalid-size-inputs-valid-test --results-path $results_dir | tail -1
+    fairseq-generate $DATA_BIN_PATH --entmax-alpha $ALPHA --batch-size 32 --path $MODEL_PATH --fixed-dictionary $DICT -s $LANG1 -t $LANG2 --remove-bpe 'sentencepiece' --beam $BEAM --task translation_multi_simple_epoch --lang-pairs $FLORES_MODEL_PATH/language_pairs.txt --decoder-langtok --encoder-langtok src --gen-subset test --fp16 --dataset-impl mmap --distributed-world-size 1 --distributed-no-spawn --skip-invalid-size-inputs-valid-test --results-path $results_dir
     cat $results_dir/generate-test.txt | grep -P '^H-'  | cut -c 3- | sort -n -k 1 | awk -F "\t" '{print $NF}' | sacrebleu $FLORES_PATH/devtest/$FLORES_LANG2.devtest --tokenize spm
 done
