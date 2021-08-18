7dd3a4d41eb6c7ce92fb0224841bf81b07d3d7dd
bpopeters <bpeters@coli.uni-saarland.de>
2021-08-18 11:46:20 +0100

    update test example creation

--- submission/make_example_data.py
+++ submission/make_example_data.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 from os.path import join
-import sys
 from itertools import permutations
 from random import shuffle
 import pickle
+import argparse
 
 
 def create_examples(multitext_ex, n_pairs):
@@ -23,23 +23,27 @@ def create_examples(multitext_ex, n_pairs):
 
 
 def main():
-    flores_path = sys.argv[1]  # path to dev data
+    parser = argparse.ArgumentParser()
+    parser.add_argument("flores_path")
+    parser.add_argument("out_path")
+    parser.add_argument("--n_examples", dtype=int, default=100)
+    parser.add_argument("--pairs_per", dtype=int, default=2)
+    opt = parser.parse_args()
     languages = ["eng", "ind", "jav", "msa", "tam", "tgl"]
 
-    n_examples = 100
-    multitext = [{"uid": str(i)} for i in range(n_examples)]
+    multitext = [{"uid": str(i)} for i in range(opt.n_examples)]
 
     for language in languages:
-        with open(join(flores_path, language + ".dev")) as f:
-            for i in range(n_examples):
+        with open(join(opt.flores_path, language + ".dev")) as f:
+            for i in range(opt.n_examples):
                 lang_ex = f.readline().strip()
                 multitext[i][language] = lang_ex
 
     output = []
     for example in multitext:
-        for ex in create_examples(example, 3):
+        for ex in create_examples(example, opt.pairs_per):
             output.append(ex)
-    with open("test_data.pickle", "wb") as f:
+    with open(opt.out_path, "wb") as f:
         pickle.dump(output, f)
 
 
