34353d1aebeac800975825aedccde7a0931e1d4b
bpopeters <bpeters@coli.uni-saarland.de>
2021-11-03 11:40:44 +0000

    make sure mop contains the alphabet

--- bathwater/mop.py
+++ bathwater/mop.py
@@ -9,30 +9,30 @@ k times in the segmented corpus.
 import sys
 import argparse
 from collections import Counter
+from itertools import chain
 
 from change_vocab import write_vocab
 
 
 def main(args):
+    # this should keep track of characters as well, because they should always
+    # be included
+    alphabet = set()
     counts = Counter()
     for line in sys.stdin:
-        counts.update(line.strip().split())
+        tokens = line.strip().split()
+        counts.update(tokens)
+        alphabet.update(chain.from_iterable(tokens))
     # now: filter
     filtered_counts = Counter(
         {t: count for t, count in counts.items() if count >= args.k}
     )
+    # after filtering, add all symbols from the alphabet
+    filtered_counts.update(alphabet)
 
     # will this have complete coverage over the alphabet? no guarantee.
     sorted_vocab = [t for t, count in filtered_counts.most_common()]
     write_vocab(sorted_vocab, opt.out)
-
-    # so, we need a method that takes a model, vocabulary, and corpus, and
-    # returns some statistics about the corpus (such as whether it differs
-    # from the original)
-
-    # so, we can use SentencePieceProcessor's set_vocabulary method (argument
-    # is a list) in order to segment differently.
-    # I think it would be best to evaluate on the dev sets.
 
 
 if __name__ == "__main__":
